ac7f90d35714f53e9b8d66624a74c14223461901
max.cembalest <max.cembalest@arthur.ai>
2023-08-31 22:24:00 -0700

    better vis layout

--- visualize_correlation.py
+++ visualize_correlation.py
@@ -19,13 +19,13 @@ def plot_human_scores_against_binary_llm_feedback(name, df, binary_feedback_col)
 	plt.savefig(f"{name}.png")
 
 def plot_human_scores_against_lettergrade_llm_feedback(name, df, lettergrade_feedback_col):
-	plt.figure(figsize=(10, 10))
+	plt.figure(figsize=(15, 5))
 	plt.suptitle(f'Human feedback vs letter grade from gpt-3.5-turbo', fontsize=20)
 	for i, col in enumerate(human_feedback_to_plot, 1):
-		plt.subplot(3, 1, i)
+		plt.subplot(1, 3, i)
 
 		# Map strings to numbers for jittering
-		unique_vals = df[lettergrade_feedback_col].unique()
+		unique_vals = ['F', 'D-', 'D', 'D+', 'C-', 'C', 'C+', 'B-', 'B', 'B+', 'A-', 'A', 'A+']
 		mapping = {val: i for i, val in enumerate(unique_vals)}
 		x_numeric = df[lettergrade_feedback_col].map(mapping)
 
@@ -33,12 +33,12 @@ def plot_human_scores_against_lettergrade_llm_feedback(name, df, lettergrade_fee
 		x_jitter = x_numeric + (np.random.rand(len(df)) * 0.2 - 0.1)  # Random values between -0.05 and 0.05
 		y_jitter = df[col] + (np.random.rand(len(df)) * 0.2 - 0.1)
 
-		sns.scatterplot(x=x_jitter, y=y_jitter, alpha=0.3)
+		sns.scatterplot(x=x_jitter, y=y_jitter, alpha=0.2)
 
 		# Set the x-ticks to original string values
 		plt.xticks(ticks=range(len(unique_vals)), labels=unique_vals)
 
-		plt.title(f'Human {col} scores \nvs\n "Is this {col.replace("nce", "nt").replace("ness", "")}?" prompted feedback')
+		plt.title(f'Human {col} scores \nvs\n "Grade this for {col}" prompted feedback')
 		plt.ylim([df[col].min(), df[col].max()])
 
 	plt.tight_layout()
